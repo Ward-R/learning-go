a90b524ea8875430b25563ef226403dbac9d6dad
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-06-24 13:04:08 +0900

    completed up to 19

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Ward-R/learning-go
 
 go 1.24.4
+
+
+// GoToElevel for udemycourse
+
+require (
+	github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
+	github.com/GoesToEleven/puppy v1.3.0 // indirect
+)
--- u-section-09/hoe-15/main.go
+++ u-section-09/hoe-15/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
 
 func main() {
-    fmt.Print("Hello World from terminal/nano")
+	fmt.Print("Hello World from terminal/nano\n")
+	s1 := puppy.Bark()
+	s2 := puppy.BigBarks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
 }
